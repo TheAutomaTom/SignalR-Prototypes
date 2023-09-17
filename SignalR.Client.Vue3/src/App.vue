eddce585991264c45ffd9ea614eab7902605d9b5
Tom Grossi <TheAutomaTom@gmail.com>
2023-09-17 13:06:27 -0500

    Folder name changes

--- SignalR.Client.Vue3/src/App.vue
+++ SignalR.Client.Vue3/src/App.vue
@@ -34,29 +34,37 @@
 </template>
 
 <script setup lang="ts">
-import * as signalR from "@microsoft/signalr"
+import * as signalR from "@microsoft/signalr";
+import HmacSHA256 from "crypto-js/hmac-sha256";
+import Base64 from "crypto-js/enc-base64";
+import Utf8 from "crypto-js/enc-utf8";
 import { ref } from "vue";
+
 
 const userInput = ref("Input...");
 const serverReply = ref("...Reply");
 
-console.log("[SignalR] Initializing connection...");
+console.log("[Client.SignalR] Building connection...");
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl("http://localhost:5192/chatHub") //SignalR-POC.ASP
+  //.withUrl("http://localhost:5192/chatHub") //SignalR-POC.ASP
+  .withUrl("http://localhost:7230/api", //Serverless running locally
+      { accessTokenFactory: () =>  generateAccessToken("Tom") }
+    )
   .configureLogging(signalR.LogLevel.Information)
   .withAutomaticReconnect()
   .build();
     
 // Inbound: `connection.on()`s should be registered before `.start` executes
 connection.on("ReceiveMessage", (reply) => {
-  console.log("[SignalR.ReceiveMessage] " + reply);
+  console.log("[Client.SignalR.ReceiveMessage] " + reply);
   serverReply.value = reply;
 });
 
 async function start() {
+    console.log("[Client.SignalR.start] starting connection...");
   try {
     await connection.start();
-    console.log("[SignalR.start] Connected.");
+    console.log("[Client.SignalR.start] Connected.");
   } catch (err) {
     console.log(err);
     setTimeout(start, 5000);
@@ -66,7 +74,7 @@ async function start() {
 // Outbound
 async function sendToServer(){
   try {
-    console.log("[SignalR.SendMessage] " + userInput.value);
+    console.log("[Client.SignalR.SendMessage] " + userInput.value);
     await connection.invoke("SendMessage", userInput.value);
   } catch (err) {
     console.error(err);
@@ -75,13 +83,48 @@ async function sendToServer(){
 
 
 connection.onclose( async () => {
-  console.log("[SignalR.onclose] Closed.");
+  console.log("[Client.SignalR.onclose] Closed.");
   await start() 
 });
 
 
 // Start the connection.
 start();
+
+
+
+
+// this function should be in auth server, do not expose your secret
+function generateAccessToken(userName: string) {
+    var header = {
+      "alg": "HS256",
+      "typ": "JWT"
+    };
+
+    var stringifiedHeader = Utf8.parse(JSON.stringify(header));
+    var encodedHeader =  Base64.stringify(stringifiedHeader); // base64url(stringifiedHeader);
+
+    // customize your JWT token payload here
+    var data = {
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": userName,
+      "exp": 1699819025,
+      'admin': true //isAdmin
+    };
+
+    var stringifiedData = Utf8.parse(JSON.stringify(data));
+    var encodedData = Base64.stringify(stringifiedData);
+
+    var token = encodedHeader + "." + encodedData;
+
+    var secret = "myFunctionAuthTest"; // do not expose your secret here
+
+    var signature = HmacSHA256(token, secret);
+    //signature = base64url(signature);
+
+    var signedToken = token + "." + signature;
+
+    return signedToken;
+  }
 </script>
 <style>
 .table2x3{
