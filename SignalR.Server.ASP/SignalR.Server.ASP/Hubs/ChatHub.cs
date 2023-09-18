efb71eded27a861c1030f09adebeffd738527b47
Tom Grossi <TheAutomaTom@gmail.com>
2023-09-18 07:14:24 -0500

    Merge!

---
+++ SignalR.Server.ASP/SignalR.Server.ASP/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_POC.ASP.Hubs;
+
+public class ChatHub : Hub
+{
+	public async Task SendMessage(string message)
+	{
+
+		char[] stringArray = message.ToCharArray();
+		Array.Reverse(stringArray);
+		string reversedStr = new string(stringArray);
+
+		await Clients.All.SendAsync("ReceiveMessage", reversedStr);
+	}
+}
---
+++ SignalR.Server.ASP/SignalR.Server.ASP/README.md
@@ -0,0 +1,7 @@
+# SignalR.Server.ASP
+
+An example of a functioning ASP SignalR server.
+
+
+
+Code mostly follows [this Microsoft Learn Tutorial ](https://learn.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-7.0&tabs=visual-studio)
